e9246aae41f301203990a452e2367e7671328163
Derek Anderson <derek.anderson@lge.com>
2014-09-25 16:57:59 -0700

    adding a deferred handler

--- samples/PromiseSample.js
+++ samples/PromiseSample.js
@@ -24,3 +24,38 @@ enyo.kind({
 
     }
 });
+
+enyo.kind({
+    name: "enyo.DeferHandler",
+    create: function(){
+        //move components off this component
+        var components = this.components;
+
+        //empty out components so they are not
+        //owned by the parent.
+        this.components = [];
+
+        //create this component as normal
+        this.inherited(arguments);
+
+        //move defered components into this component
+        //so they are owned by this def
+        this.createComponents(components);
+    },
+    catchEvent: function() {
+        console.log('caught in defer');
+    }
+});
+
+enyo.kind({
+    name: "enyo.sample.DeferHandlerSample",
+    components: [{
+        kind: "enyo.DeferHandler",
+        components:[
+            {kind:"Button", content: "Test Button", ontap: "catchEvent"}
+        ]
+    }],
+    catchEvent: function(sender, event) {
+        console.log(sender);
+    }
+});
